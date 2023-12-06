4084debce9a5a8c53a0e7dd42a7b7e8e6750faef
Anwooseong <aws5624@naver.com>
2023-12-06 15:15:32 +0900

    Fix: Spring security 수정

--- src/main/java/com/ll/medium/global/security/SecurityConfig.java
+++ src/main/java/com/ll/medium/global/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ll.medium.global.security;
 
+import com.ll.medium.domain.member.service.CustomAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,13 +20,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests.requestMatchers("/**")
-                                .permitAll()
-                )
                 .formLogin(
                         form ->
-                                form.loginPage("/members/login")
+                                form
+                                        .loginPage("/members/login")
                                         .defaultSuccessUrl("/")
                                         .usernameParameter("loginId")
                                         .failureUrl("/members/login/error")
@@ -47,6 +45,18 @@ public class SecurityConfig {
                                 csrf.ignoringRequestMatchers(
                                         "/h2-console/**"
                                 )
+                )
+                .authorizeHttpRequests(
+                        auth ->
+                                auth
+                                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
+                                        .requestMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
+                                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(
+                        except ->
+                                except.authenticationEntryPoint(new CustomAuthenticationEntryPoint())
                 )
         ;
 
